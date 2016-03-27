8ccf0e2c591bdf52bbc417b916501d00f1c9d90e
tutorial <mouse0407@163.com>
2016-03-27 23:37:07 +0800

    Added table City to the database

---
+++ tutorial/db/up/002CreateTableCity.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE City (
+    "CityID"          INT        NOT NULL,
+    "CityName"        NCHAR (50) NOT NULL,
+    "CityDescription" NCHAR (50) NOT NULL,
+    CONSTRAINT "PK_City" PRIMARY KEY NONCLUSTERED ("CityID" ASC)
+);
