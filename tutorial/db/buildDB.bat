1c6407a7b4ff13caf32f461185dda779cc81278b
tutorial <mouse0407@163.com>
2016-03-27 17:42:43 +0800

    fix path issue

--- tutorial/db/buildDB.bat
+++ tutorial/db/buildDB.bat
@@ -1,10 +1,8 @@
 @echo off
 SET DIR=%~d0%~p0%
 
-SET sql.files.directory="%DIR%"
-SET version.file="%DIR%_BuildInfo.xml"
 SET connstring="data source=TUTORIAL\TUTORIAL;initial catalog=Northwind;integrated security=True"
 SET repository.path="https://github.com/espiderinc/tutorial-demo-net.git"
  
-"%DIR%..\..\packages\roundhouse.0.8.6\bin\rh.exe" /f=%sql.files.directory% /c=%connstring% /vf=%version.file% /r=%repository.path% /env=LOCAL /ni /drop
-"%DIR%..\..\packages\roundhouse.0.8.6\bin\rh.exe" /f=%sql.files.directory% /c=%connstring% /vf=%version.file% /r=%repository.path% /env=LOCAL /ni /simple
+"%DIR%..\..\packages\roundhouse.0.8.6\bin\rh.exe" /c=%connstring% /r=%repository.path% /env=LOCAL /ni /drop
+"%DIR%..\..\packages\roundhouse.0.8.6\bin\rh.exe" /c=%connstring% /r=%repository.path% /env=LOCAL /ni /simple
