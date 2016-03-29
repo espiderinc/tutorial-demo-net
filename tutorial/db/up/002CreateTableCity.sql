a6986bea050c87500c521f3851f56f33e17f50ca
tutorial <mouse0407@163.com>
2016-03-29 22:12:19 +0800

    add new column to table City

--- tutorial/db/up/002CreateTableCity.sql
+++ tutorial/db/up/002CreateTableCity.sql
@@ -2,5 +2,6 @@
     "CityID"          INT        NOT NULL,
     "CityName"        NCHAR (50) NOT NULL,
     "CityDescription" NCHAR (50) NOT NULL,
+	"Country"		  NCHAR (50) Not NULL,
     CONSTRAINT "PK_City" PRIMARY KEY NONCLUSTERED ("CityID" ASC)
 );
