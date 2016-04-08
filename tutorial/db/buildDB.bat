0de7245fe1f7531335eb56bd9bfe15034df6609d
tutorial <mouse0407@163.com>
2016-04-09 00:28:49 +0800

    change db update strategy from rebuild to upgrade.

--- tutorial/db/buildDB.bat
+++ tutorial/db/buildDB.bat
@@ -4,5 +4,4 @@ SET DIR=%~d0%~p0%
 SET connstring="data source=TUTORIAL\TUTORIAL;initial catalog=Northwind;integrated security=True"
 SET repository.path="https://github.com/espiderinc/tutorial-demo-net.git"
  
-"%DIR%..\..\packages\roundhouse.0.8.6\bin\rh.exe" /c=%connstring% /vf=../bin/tutorial.dll /r=%repository.path% /env=LOCAL /ni /drop
 "%DIR%..\..\packages\roundhouse.0.8.6\bin\rh.exe" /c=%connstring% /vf=../bin/tutorial.dll /r=%repository.path% /env=LOCAL /ni /simple
