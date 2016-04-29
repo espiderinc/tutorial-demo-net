90b8e4c52c245c8b4fb23644767b02504516a281
tutorial <mouse0407@163.com>
2016-04-29 22:24:00 +0800

    fix test

---
+++ tutorial/Controllers/tests/ProductControllerTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using tutorial.Controllers;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace tutorial.Tests.Controllers
+{
+    [TestClass]
+    public class ProductControllerTest
+    {
+        [TestMethod]
+        public void GetAll()
+        {
+            // Arrange
+            ProductController controller = new ProductController();
+
+            // Act
+            IQueryable<Product> result = controller.Get();
+            
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(77, result.Count());
+        }
+
+        [TestMethod]
+        public void GetById()
+        {
+            // Arrange
+            ProductController controller = new ProductController();
+
+            // Act
+            Product product = controller.Get(5);
+
+            // Assert
+            Assert.IsNotNull(product);
+            Assert.AreEqual("Chef Anton's Gumbo Mix", product.ProductName);
+        }
+    }
+}
