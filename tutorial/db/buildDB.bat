6874e484678eaa3ff3e6757260957690295bf59c
tutorial <mouse0407@163.com>
2016-05-01 22:21:44 +0800

    revert the change

--- tutorial/db/buildDB.bat
+++ tutorial/db/buildDB.bat
@@ -4,4 +4,4 @@ SET DIR=%~d0%~p0%
 SET connstring="data source=TUTORIAL\TUTORIAL;initial catalog=Northwind;integrated security=True"
 SET repository.path="https://github.com/espiderinc/tutorial-demo-net.git"
  
-"C:\Users\lemurzjb\Downloads\RoundhousE.v0.8.6.0\RoundhousE\console\rh.exe" /c=%connstring% /vf=../bin/tutorial.dll /r=%repository.path% /env=LOCAL /ni /simple
+"%DIR%..\..\packages\roundhouse.0.8.6\bin\rh.exe" /c=%connstring% /vf=../bin/tutorial.dll /r=%repository.path% /env=LOCAL /ni /simple
